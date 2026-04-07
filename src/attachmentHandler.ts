26fa515d3f4cf63982dcc8ac5a611cda2210113c
Nikolai Maslak <nikolai.maslak@brightpattern.com>
2026-04-07 11:33:54 -0700

    Add support for .HAR file previews with click-to-open functionality

--- src/attachmentHandler.ts
+++ src/attachmentHandler.ts
@@ -20,6 +20,38 @@ export async function handleAttachments() {
 
             const attachmentUrl = attachmentLinkEl.href
 
+            if (attachmentUrl.endsWith('.har')) {
+                addStyle('better-trac-har', `
+                        .better-trac-har {
+                            max-height: 400px;
+                            border: 1px solid #ccc;
+                            width: 100%;
+                            padding: 4px;
+                            overflow: auto;
+                            display: flex;
+                            flex-direction: column;
+                            gap: 4px;
+                            cursor: pointer;
+                        }
+                `)
+
+                const harlLinkEl = createLayoutFromString(`
+                    <div class="better-trac-har">
+                        Open .HAR
+                    </div>
+                `)
+
+                harlLinkEl.addEventListener('click', async () => {
+                    const res = await fetch(attachmentUrl);
+                    const fileContentBuffer = new Uint8Array(await res.arrayBuffer());
+                    openInBrowser(fileContentBuffer);
+                });
+
+                attachmentLinkEl.parentElement?.insertBefore(harlLinkEl, attachmentLinkEl)
+
+                return
+            }
+
             const attachmentMimeType = await fetchMimeType(attachmentUrl)
 
             if (attachmentMimeType?.startsWith('video/')) {
@@ -55,6 +87,7 @@ export async function handleAttachments() {
                             display: flex;
                             flex-direction: column;
                             gap: 4px;
+                            cursor: pointer;
                         }
                     `)
 
